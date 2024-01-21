0e8bd964cc476a3eb24621fa8899c4d1e76c332d
gatikrushna911 <gatikrushna911@gmail.com>
2024-01-21 15:28:05 +0530

    user context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./components/Body";
 import Header from "./components/Header";
@@ -6,13 +6,26 @@ import AboutUS from "./components/AboutUS";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import ContactUs from "./components/ContactUs";
 import RestaurantMenu from "./components/RestaurantMenu";
+import UserContext from "./components/UserContext";
 
 const AppLayout = () => {
+  const [userName, setUserName] = useState();
+
+  useEffect(() => {
+    const data = {
+      name: "Gatikrushna",
+    };
+    setUserName(data);
+  }, []);
+
   return (
-    <div>
-      <Header />
-      <Outlet />
-    </div>
+    //wanted to pass the context data to the whole application
+    <UserContext.Provider value={{ loggedInUser: userName }}>
+      <div>
+        <Header />
+        <Outlet />
+      </div>
+    </UserContext.Provider>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import { HEADER_LOGO } from "../utils/Constants";
+import { useContext } from "react";
+import UserContext from "./UserContext";
 const Header = () => {
+  // by this we can add the context individually to the each page
+  const { loggedInUser } = useContext(UserContext);
   return (
     //header-container
     <div className="flex justify-between shadow-lg border border-solid">
@@ -20,6 +24,7 @@ const Header = () => {
             <Link to="contact">Contact Us</Link>
           </li>
           <li className="m-2 p-2">Cart</li>
+          <li className="m-2 p-2 text-red-400">{loggedInUser}</li>
         </ul>
       </div>
     </div>
